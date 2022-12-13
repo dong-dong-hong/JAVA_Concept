4143653b6754e1ffd52fcb9396598feb49dde0fa
dong-dong-hong <87763201+dong-dong-hong@users.noreply.github.com>
2022-12-13 11:24:32 +0900

    Update Main.java

--- day18/src/ioQuiz/Main.java
+++ day18/src/ioQuiz/Main.java
@@ -2,33 +2,24 @@ package ioQuiz;
 import java.util.Scanner;
 
 public class Main {
-	
-	
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method
-		 Somi somi = new Somi();
-		 Duchi duchi = new Duchi();                                  
+		
+		                                   
 		 Scanner scanner = new Scanner(System.in);
 		 System.out.println("부르고 싶은 동물의 이름을 불러주세요.");
 		 String a = scanner.next();
 		 
-		if(a == "소미" ) {
+		if(a.equlas("소미") ) {
+			Somi somi = new Somi();
 			somi.called();
-		}else if(a == "두치") {
+		}else if(a.equlas("두치")) {
+			Duchi duchi = new Duchi();
 			duchi.called();
 		}else {
 			System.out.println("실패");
 		}
 		
-		// 부르고 싶은 동물의 이름을 불러주세요.
-		// 소미
-		// 소미: 나 불렀냐옹?
 		
-		// 부르고 싶은 동물의 이름을 불러주세요.
-		// 두치
-		// 두치: 날 불렀나?
-		
-		// 아직 실패..
 }
 }
