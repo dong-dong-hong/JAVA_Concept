459685e382d6551d06fe8fc50c3ab7ce217bc73d
dong-dong-hong <87763201+dong-dong-hong@users.noreply.github.com>
2022-12-13 13:12:07 +0900

    Update DogArmor.java

--- day19/src/genericsQuiz2/DogArmor.java
+++ day19/src/genericsQuiz2/DogArmor.java
@@ -2,14 +2,8 @@ package genericsQuiz2;
 
 public class DogArmor<T extends Dog> {
 
-	T armor;
-	
-	public void setArmor(T armor) {
-		System.out.println("갑옷 장착 완료");
-	}
-	
-	public T getArmor() {
-		return armor;
+	public void setArmor(String name){
+		System.out.pritln(name + " : 갑옷 장착 완료");
 	}
 	
 	
